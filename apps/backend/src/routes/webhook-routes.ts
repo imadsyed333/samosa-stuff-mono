6c1fbab92159b85cc70aa770ba794e4183101d0a
Imad Syed <64811136+imadsyed333@users.noreply.github.com>
2026-01-28 00:04:09 -0500

    Removing payment and webhooks (#3)
    * Removed unused imports and variables
    
    * Editing if-dondition to utilize primitive type selectProduct.id to match useEffect dependency array
    
    * Removing unused import
    
    * Using object in dependency array to ensure build completes (will find fix later)
    
    * ignoring build file
    
    * Temporarily disabled use of webhook-routes, causing build errors
    
    * Removed unused files and all traces of payment and webhook routes and controllers

--- apps/backend/src/routes/webhook-routes.ts
+++
@@ -1,8 +0,0 @@
-// import { raw, Router } from "express";
-// import { handleCheckout } from "../controllers/webhook-controller";
-
-// const router = Router();
-
-// router.post("/", raw({ type: "application/json" }), handleCheckout);
-
-// export default router;
