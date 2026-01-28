a0a02a6f50b8a2495c0a8814b4e132a43edef490
imadsyed333 <creeperone23@gmail.com>
2026-01-27 22:18:26 -0500

    Removing unused import

--- apps/frontend/src/context/AuthContext.tsx
+++ apps/frontend/src/context/AuthContext.tsx
@@ -1,10 +1,4 @@
-import React, {
-  createContext,
-  useContext,
-  useEffect,
-  useMemo,
-  useState,
-} from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import { FieldErrors, User } from "../lib/types";
 import {
   loginUser,
