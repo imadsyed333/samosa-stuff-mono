6b6e986a528a4b9e92647e8d542b2d75b529ee2b
Imad Syed <64811136+imadsyed333@users.noreply.github.com>
2026-02-03 19:12:50 -0500

    sameSite=True (#8)
    * Removed unused imports and variables
    
    * Editing if-dondition to utilize primitive type selectProduct.id to match useEffect dependency array
    
    * Removing unused import
    
    * Using object in dependency array to ensure build completes (will find fix later)
    
    * ignoring build file
    
    * Temporarily disabled use of webhook-routes, causing build errors
    
    * Removed unused files and all traces of payment and webhook routes and controllers
    
    * Updated build command to use backend tsconfig file
    
    * Changes to cookie options to ensure cookies are being sent to frontend
    
    * Implemented local loading state to match RegisterPage, ensuring button is not in loading state upon page load
    
    * Set Inter as default font across devices
    
    * Disable AddToCart button if user not logged in
    
    * Basic input validation for phone number
    
    * dev-serve-static (#7)
    
    * Added "/api" prefix to api routes, serving static frontend build files
    
    * Added homepage value to support routing through static files
    
    * Added more product images
    
    * Setting sameSite cookies to strict to prevent issues in iOS browsers

--- apps/backend/src/utils/constants.ts
+++ apps/backend/src/utils/constants.ts
@@ -2,7 +2,7 @@ export const ACCESS_COOKIE_OPTIONS = {
   httpOnly: true,
   sameSite:
     process.env.NODE_ENV === "production"
-      ? ("none" as const)
+      ? ("strict" as const)
       : ("lax" as const),
   secure: process.env.NODE_ENV === "production",
   maxAge: 60 * 60 * 1000,
@@ -12,7 +12,7 @@ export const REFRESH_COOKIE_OPTIONS = {
   httpOnly: true,
   sameSite:
     process.env.NODE_ENV === "production"
-      ? ("none" as const)
+      ? ("strict" as const)
       : ("lax" as const),
   secure: process.env.NODE_ENV === "production",
   maxAge: 30 * 24 * 60 * 60 * 1000,
